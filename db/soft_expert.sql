ac0df57f4154d818310e8db729d59a55f2248685
Thiago Bomfim <thiagobs.webdev@gmail.com>
2020-03-16 11:08:00 -0300

    Alter file sql

--- db/soft_expert.sql
+++ db/soft_expert.sql
@@ -1,6 +1,8 @@
-drop table users;
+begin;
+
+DROP TABLE IF EXISTS users CASCADE;
 -- create table users
-CREATE TABLE if exists users(
+CREATE TABLE users(
 	id serial PRIMARY KEY,	
 	first_name VARCHAR(255) NOT NULL,
 	last_name VARCHAR(255) NOT NULL,
@@ -14,13 +16,12 @@ CREATE TABLE if exists users(
 	updated_at TIMESTAMP
 	);
 
--- inserction 
-begin
+-- inserction
 INSERT INTO users (first_name, last_name, email, passwd, created_at) VALUES ('Thiago','Bomfim', 'thiagobs.webdev@gmail.com', '$2y$10$p/0C8Ct4W71SfrVSqUZQXOZzYbPALi3eul6t.Y/yZFZYTKD2dz2gi', NOW());
-commit
 
 
-drop table if exists product_type_taxes;
+
+DROP TABLE IF EXISTS product_type_taxes CASCADE;
 -- create table product_type_taxes
 CREATE TABLE product_type_taxes(
 	id serial PRIMARY KEY,	
@@ -32,14 +33,13 @@ CREATE TABLE product_type_taxes(
 );
 
 -- inserction 
-begin
 INSERT INTO product_type_taxes (name, percentage, registered_by_id, created_at) VALUES ('ICMS', 6.50, 1, NOW());
 INSERT INTO product_type_taxes (name, percentage, registered_by_id, created_at) VALUES ('Cofins', 3.60, 1, NOW());
 INSERT INTO product_type_taxes (name, percentage, registered_by_id, created_at) VALUES ('Cide', 2.35, 1, NOW());
 INSERT INTO product_type_taxes (name, percentage, registered_by_id, created_at) VALUES ('ITR', 5.25, 1, NOW());
-commit
 
-drop table if exists product_types;
+
+DROP TABLE IF EXISTS product_types CASCADE;
 -- create table product_types
 CREATE TABLE product_types(
 	id serial PRIMARY KEY,	
@@ -51,15 +51,14 @@ CREATE TABLE product_types(
 );
 
 -- inserction 
-begin
 INSERT INTO product_types (name, product_type_taxes_id, registered_by_id, created_at) VALUES ('Informática', 1, 1, NOW());
 INSERT INTO product_types (name, product_type_taxes_id, registered_by_id, created_at) VALUES ('Cozinha', 2, 1, NOW());
 INSERT INTO product_types (name, product_type_taxes_id, registered_by_id, created_at) VALUES ('Eletrodomésticos', 3, 1, NOW());
 INSERT INTO product_types (name, product_type_taxes_id, registered_by_id, created_at) VALUES ('Esportes', 4, 1, NOW());
-commit
+
 
 -- inserction 
-drop table if exists products;
+DROP TABLE IF EXISTS products CASCADE;
 -- create table products
 CREATE TABLE products(
 	id serial PRIMARY KEY,	
@@ -72,14 +71,12 @@ CREATE TABLE products(
 );
 
 -- inserction 
-begin
 INSERT INTO products (name, price, product_type_id, registered_by_id, created_at) VALUES ('Geladeira', 1500, 2, 1, NOW());
 INSERT INTO products (name, price, product_type_id, registered_by_id, created_at) VALUES ('Mouse', 55.60, 1, 1, NOW());
 INSERT INTO products (name, price, product_type_id, registered_by_id, created_at) VALUES ('Fogão', 540, 3, 1, NOW());
 INSERT INTO products (name, price, product_type_id, registered_by_id, created_at) VALUES ('Prancha', 1100, 4, 1, NOW());
-commit
 
-drop table if exists sales;
+DROP TABLE IF EXISTS sales CASCADE;
 -- create table products
 CREATE TABLE sales(
 	id serial PRIMARY KEY,
@@ -91,11 +88,11 @@ CREATE TABLE sales(
 );
 
 -- inserction 
-begin
 INSERT INTO sales (product_id, amount, registered_by_id, created_at) VALUES (4, 2, 1, NOW());
 INSERT INTO sales (product_id, amount, registered_by_id, created_at) VALUES (1, 2, 1, NOW());
 INSERT INTO sales (product_id, amount, registered_by_id, created_at) VALUES (3, 4, 1, NOW());
 INSERT INTO sales (product_id, amount, registered_by_id, created_at) VALUES (2, 22, 1, NOW());
-commit
+
+commit;
 
 
